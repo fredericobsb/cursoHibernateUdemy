d9c039e798d2ed78d88bf3236c7a6c7bb4c97733
Frederico - Dev Full Stack <frederico0707gmail.com>
2021-12-31 01:19:29 -0300

    Aula 80 - Criteria - Restrictions

--- src/main/java/com/infiniteskills/data/HibernateApplication.java
+++ src/main/java/com/infiniteskills/data/HibernateApplication.java
@@ -1,10 +1,13 @@
 package com.infiniteskills.data;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 import com.infiniteskills.data.entities.Transaction;
 
@@ -21,8 +24,13 @@ public class HibernateApplication {
 			session = factory.openSession();
 			tx = session.beginTransaction();
 
+			Criterion criterion1 = Restrictions.le("amount", new BigDecimal(
+					"20.00"));
+			Criterion criterion2 = Restrictions.eq("transactionType",
+					"Withdrawl");
+
 			List<Transaction> transactions = session
-					.createCriteria(Transaction.class)
+					.createCriteria(Transaction.class).add(Restrictions.and(criterion1, criterion2))
 					.addOrder(Order.desc("title")).list();
 
 			for (Transaction t : transactions) {
@@ -30,6 +38,7 @@ public class HibernateApplication {
 			}
 
 			tx.commit();
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
